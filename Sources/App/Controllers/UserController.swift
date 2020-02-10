cdaa94ed32e6d9648ea3d287453a037ee91f1a9b
Jefry Eko Mulya <jefrydagucci@gmail.com>
2020-02-11 02:37:27 +0700

    [V-005] Create Get request to database

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -15,6 +15,7 @@ struct UserController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let userRoutes = routes.grouped("api", "users")
         userRoutes.post(use: create)
+        userRoutes.get(use: getAll)
     }
     
     /*
@@ -27,6 +28,23 @@ struct UserController: RouteCollection {
     func create(req: Request) throws -> EventLoopFuture<User> {
         let user = try req.content.decode(User.self)
         return user.save(on: req.db).map { user }
+    }
+    
+    /*
+     GET {{host}}/api/users/
+     [
+     {
+     "name" : "Jefry",
+     "username": "jefrydagucci"
+     },
+     {
+     "name" : "Jefry2",
+     "username": "jefrydagucci2"
+     }
+     ]
+     */
+    func getAll(req: Request) throws -> EventLoopFuture<[User]> {
+        return User.query(on: req.db).all()
     }
 }
 
